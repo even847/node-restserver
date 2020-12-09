f122c2c71f524eb60ef31bb1a90bfd6a082cca28
Edwin Aponte <aponte847@gmail.com>
2020-12-09 16:56:29 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Even847:AtkQtU5tmMd6F35Q@cluster0.bb7g6.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URL
 }
 
 process.env.URLDB = urlDB;
