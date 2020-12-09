dd9d796f9e2c20687ee1c78b466150dff0d53121
Edwin Aponte <aponte847@gmail.com>
2020-12-09 15:01:36 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 // ============
 process.env.PORT = process.env.PORT || 3000;
+
+// ============
+// Entorno
+// ============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============
+// Base de Datos
+// ============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Even847:AtkQtU5tmMd6F35Q@cluster0.bb7g6.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,37 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use( require('./routes/usuario') );
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario')
-});
-
-app.post('/usuario', (req, res) => {
-    let body = req.body;
-
-    if (body.nombre === undefined ) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
+mongoose.connect(process.env.URLDB, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        }, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('Base de Datos online');
+ 
         });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto:', process.env.PORT);
